dfdf611dbbb6d71f49853c4d08833437e6a20507
Juan <juan_jg@hotmail.com>
2024-01-27 18:48:39 -0300

    test import addon

--- src/renderer/src/components/MainWindow.tsx
+++ src/renderer/src/components/MainWindow.tsx
@@ -1,3 +1,13 @@
+// import addon from '../../../../addon/build/Release/hello_addon.node'
+// const addon = require('../../../../addon/build/Release/hello_addon.node')
+
 export function MainWindow() {
-  return <div className="text-red-600 border">Spectrum Analizer</div>
+  // const test = addon.hello()
+  // console.log(addon.hello())
+  return (
+    <div className="text-red-600 border">
+      Spectrum Analizer
+      {/* <span>test</span> */}
+    </div>
+  )
 }
